fa01dd8cc29c3ad6cff0b658fe783cba9876a70f
Константин <raverus@inbox.ru>
2021-06-06 20:51:58 +0700

    Добавлено редактирование заголовков узлов дерева

---
+++ TestFullStackDev/InputBox.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+
+namespace TestFullStackDev
+{
+    /// <summary>
+    /// Логика взаимодействия для InputBox.xaml
+    /// </summary>
+    public partial class InputBox : Window
+    {
+        public InputBox(string message)
+        {
+            InitializeComponent();
+            Message_TextBlock.Text = message;
+
+            TextBox.KeyUp += (s, a) =>
+            {
+                if (string.IsNullOrEmpty(TextBox.Text))
+                {
+                    Validation_TextBlock.Visibility = Visibility.Visible;
+                    Ok_Button.IsEnabled = false;
+                }
+                else
+                {
+                    Validation_TextBlock.Visibility = Visibility.Hidden;
+                    Ok_Button.IsEnabled = true;
+                }
+            };
+        }
+
+        private void Ok_Button_Click(object sender, RoutedEventArgs e) =>
+            DialogResult = true;
+
+        private void Cancel_Button_Click(object sender, RoutedEventArgs e) =>
+            DialogResult = false;
+    }
+}
--- TestFullStackDev/MainWindow.xaml.cs
+++ TestFullStackDev/MainWindow.xaml.cs
@@ -11,33 +11,49 @@ namespace TestFullStackDev
         public MainWindow()
         {
             InitializeComponent();
+            
+            TV.SelectedItemChanged += (s, a) =>
+            {
+                if (a.NewValue == null)
+                {
+                    Remove_Button.IsEnabled = false;
+                    Edit_Button.IsEnabled = false;
+                }
+                else
+                {
+                    Remove_Button.IsEnabled = true;
+                    Edit_Button.IsEnabled = true;
+                }
+            };
         }
 
         private void Add_Button_Click(object sender, RoutedEventArgs e)
         {
+            //Если есть выбранный элемент, то добавляем новый в его вложение
             if (TV.SelectedItem is TreeViewItem selectedItem)
             {
                 selectedItem.Items.Add(new TreeViewItem
                 {
                     Header = $"{selectedItem.Header}_{selectedItem.Items.Count + 1}"
                 });
+
+                selectedItem.IsExpanded = true;
             }
+            //В противном случае в корень
             else
             {
                 TV.Items.Add(new TreeViewItem
                 {
-                    Header =$"Node {TV.Items.Count + 1}"
+                    Header = $"Node {TV.Items.Count + 1}"
                 });
             }
         }
 
         private void Remove_Button_Click(object sender, RoutedEventArgs e)
         {
-            //Проверка, что есть выбранный элемент
             if (TV.SelectedItem == null) return;
 
             var selectedItem = (TreeViewItem) TV.SelectedItem;
-
             if (selectedItem.Parent is TreeViewItem parent)
             {
                 parent.Items.Remove(selectedItem);
@@ -50,13 +66,20 @@ namespace TestFullStackDev
 
         private void Edit_Button_Click(object sender, RoutedEventArgs e)
         {
+            if (TV.SelectedItem == null) return;
 
+            var inputBox = new InputBox("Enter a new header here.");
+            var result = inputBox.ShowDialog();
+            
+            //Если была нажата кнопка Cancel
+            if (result != true) return;
+
+            var selectedItem = (TreeViewItem)TV.SelectedItem;
+            selectedItem.Header = inputBox.TextBox.Text;
         }
 
-        private void Reset_Button_Click(object sender, RoutedEventArgs e)
-        {
+        private void Reset_Button_Click(object sender, RoutedEventArgs e) =>
             TV.Items.Clear();
-        }
 
         //Убрать фокус, чтобы добавить элемент в корень дерева
         private void TV_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
